458e046fd24288e6b73f6e3cbcc1ae4bf69d8851
Rahmatov Avazbek <117166824+RahmatovAvazbek@users.noreply.github.com>
2022-11-01 16:13:54 +0500

    Add files via upload

---
+++ 1-topshiriq.py
@@ -0,0 +1,45 @@
+# UY ISHI
+
+#"Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+#print(' "Nexia", "Tico" ', "'Damas'  ko'rganlar qilar havas")
+
+# 5 ning 4-darajasini toping
+
+#print('5 ning 4-darajasi :', 5**4 , 'ga teng bo`ladi ')
+
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+
+# 1 - usul
+#a = int(input('Surat uchun  Natural son kiriting : '))
+#b = int(input('maxraj uchun natural son kiriting : ')) 
+#c = a // b 
+#d = a - b * c
+#print(a , ' ni ' , b , ' ga bo`lganda ' , d , ' qoldiq qoladi ')
+
+# 2 - usul
+#a = int(input('Surat uchun  Natural son kiriting : '))
+#b = int(input('maxraj uchun natural son kiriting : ')) 
+#d = a % b
+#print(a , ' ni ' , b , ' ga bo`lganda ' , d , ' qoldiq qoladi ') 
+
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+
+#a = 125 
+#p = 4 * a 
+#s = a**2
+#print (' kvadratning perimetri  ' , p , ' ga ' , '\n yuzi esa ' , s , ' ga teng ')
+
+#Diametri 12 ga teng bo'lgan doiraning yuzini toping  ( deb oling)
+
+#d = 12 
+#PI = 3.14
+#s = PI * d**2/4 
+#print('diametri 12 ga teng bo`lgan doira yuzi ', s , ' ga teng ')
+
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+
+#a = 6
+#b = 7 
+#c = (a **2 + b**2)**(1/2)
+#print(' katetlari 6 va 7 ga teng bo`lgan to`g`ri burchakli uchburchakning gipotenuzasi ' , c , ' ga teng ')
+
